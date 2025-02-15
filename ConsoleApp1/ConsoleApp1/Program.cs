ea573f0ec33c38ab31e657699c2d82ad9caddf8b
DEIVID ALBERTO GUERRA CARPIO <dguerrac7@miumg.edu.gt>
2025-02-15 10:52:56 -0600

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+Console.WriteLine("Ingrese  dos numeros");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Que numero desea hacer con los numeros");
+Console.WriteLine("presione 1 para sumar");
+Console.WriteLine("presione 2 para restar");
+Console.WriteLine("presione 3 para multiplicar");
+Console.WriteLine("presione 3 para dividir");
+
+int numsw = int.Parse(Console.ReadLine());
+
+switch (numsw)
+{
+    case 1:
+        Console.WriteLine($" La suma es: {num1 + num2} ");
+        break;
+    case 2:
+        Console.WriteLine($" La resta es: {num1 - num2} ");
+        break;
+
+    case 3:
+        Console.WriteLine($" La multiplicacion es: {num1 * num2} ");
+        break;
+    case 4:
+        Console.WriteLine($" La division es: {num1 / num2} ");
+        break;
+    default: Console.WriteLine("adios"); break;
+}
